42c4e182fb41c8ac29a7845bdf19e6422bb35a18
Bernhard, Dave(AWF) <dbernhard@ebay.com>
2020-11-03 14:28:35 +0000

    feat: add category selects

---
+++ src/components/CategorySelect.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import type { Category } from '../pages/Drawing.svelte'
+
+    export let label: string
+    export let boundOption: string = ''
+    export let categories: Category[]
+</script>
+
+<label for={label} />
+<select bind:value={boundOption} id={label}>
+    {#each categories as category}
+        <option value={category.value}>{category.text}</option>
+    {/each}
+</select>
--- src/pages/Drawing.svelte
+++ src/pages/Drawing.svelte
@@ -1,5 +1,43 @@
+<!-- context="module" is needed to export TS types -->
+<script context="module" lang="ts">
+    export interface Category {
+        value: string
+        text: string
+    }
+</script>
+
 <script lang="ts">
+    import CategorySelect from '../components/CategorySelect.svelte'
+
+    let firstOption: string
+    let secondOption: string
+    let thirdOption: string
+
+    let categories = [
+        { value: 'visual-style', text: 'Visual style' },
+        { value: 'principal', text: 'Principal' },
+        { value: 'object', text: 'Object' },
+        { value: 'mood', text: 'Mood' },
+        { value: 'abstract-concept', text: 'Abstract concept' }
+    ]
+
+    function generate() {
+        console.log('generate')
+    }
 </script>
 
 <nav><a href="/dev">Web dev prompts</a> <a href="/about">About</a></nav>
 <h1>Hello from Drawing</h1>
+
+<h2>Categories</h2>
+
+<!-- {categories} is the same as saying categories={categories} -->
+<CategorySelect label="first-option" bind:boundOption={firstOption} {categories} />
+<CategorySelect label="second-option" bind:boundOption={secondOption} {categories} />
+<CategorySelect label="third-option" bind:boundOption={thirdOption} {categories} />
+
+<p>First: {firstOption}</p>
+<p>Second: {secondOption}</p>
+<p>Third: {thirdOption}</p>
+
+<button on:click={generate}>GO</button>
