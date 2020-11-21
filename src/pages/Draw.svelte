cb0bea53720c704a5a4d4be947c37bd9101629ed
Maggie Appleton <maggie.fm.appleton@gmail.com>
2020-11-21 23:13:44 +0000

    much css

--- src/pages/Draw.svelte
+++ src/pages/Draw.svelte
@@ -96,12 +96,16 @@
     </div>
     {/each}
   
-  <button id="addPromptButton" on:click={addPrompt}>
-      <svg width="40px" height="40px" viewBox="0 0 40 40" tabindex="0" aria-hidden="true">
-          
-      </svg>
-      <span class="sr-only">Add a prompt</span>
+<div class="addPrompt">
+  <span>Add a prompt</span>
+  <button on:click={addPrompt}>
+    <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg" tabindex="0" aria-hidden="true">
+        <circle cx="18" cy="18" r="18" />
+        <rect x="8.52637" y="20.915" width="5.82996" height="18.9474" transform="rotate(-90 8.52637 20.915)" fill="rgb(152, 156, 187)"/>
+        <rect x="21.0317" y="27.4738" width="6.06316" height="18.9474" transform="rotate(180 21.0317 27.4738)" fill="rgb(152, 156, 187)"/>
+        </svg>
   </button>
+</div>
 
   <button class="button" on:click={generate}>Generate!</button>
 </div>
