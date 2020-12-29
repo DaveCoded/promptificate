45f62a3404c6379abd15bcad818ee5e2e936fc23
Dave Bernhard <dave.bernhard@feedagency.com>
2020-12-29 12:54:38 +0000

    feat: add underline to active link in nav

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,2 +1,16 @@
+<script lang="ts">
+    let path = window.location.href;
+</script>
 
-<nav><a href="/">Home</a><a href="/develop">Develop</a> <a href="/draw">Draw</a> <a href="/about">About</a></nav>
+<style>
+    .activeNav {
+        text-decoration: underline;
+    }
+</style>
+
+<nav>
+    <a href="/" class:activeNav={path.endsWith('/')}>Home</a>
+    <a href="/develop" class:activeNav={path.endsWith('/develop')}>Develop</a>
+    <a href="/draw" class:activeNav={path.endsWith('/draw')}>Draw</a>
+    <a href="/about" class:activeNav={path.endsWith('/about')}>About</a>
+</nav>
