db48266226586ae0b7ef7bae08b85f30fe817f1a
Maggie Appleton <maggie.fm.appleton@gmail.com>
2021-01-03 17:11:02 +0000

    styling close cross

--- src/components/CloseCross.svelte
+++ src/components/CloseCross.svelte
@@ -1,6 +1,22 @@
+<script>
+export let isLocked
+</script>
+
+<style>
+
+.crossPath {
+    transition: all 0.3s ease-out;
+    stroke: #878CB0;
+}
+
+.active {
+    stroke: #d1d4e4;
+}
+
+</style>
+
 <svg width="24" height="24" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <!-- <path d="M32 17C32 8.71875 25.2812 2 17 2C8.71875 2 2 8.71875 2 17C2 25.2812 8.71875 32 17 32C25.2812 32 32 25.2812 32 17Z" stroke="rgb(152, 156, 187)" stroke-width="3" stroke-miterlimit="10"/> -->
-    <path d="M23.25 23.25L10.75 10.75" stroke="rgb(152, 156, 187)" stroke-width="3" stroke-linecap="round"/>
-    <path d="M10.75 23.25L23.25 10.75" stroke="rgb(152, 156, 187)" stroke-width="3" stroke-linecap="round"/>
+    <path class={!isLocked ? 'crossPath' : 'crossPath active'} d="M23.25 23.25L10.75 10.75" stroke-width="3" stroke-linecap="round"/>
+    <path class={!isLocked ? 'crossPath' : 'crossPath active'} d="M10.75 23.25L23.25 10.75" stroke-width="3" stroke-linecap="round"/>
     </svg>
     
